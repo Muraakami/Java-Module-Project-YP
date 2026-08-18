642c227176b4a11c0ee4278a4b278ae2eb49080f
Muraakami_ <Danil_16101998@mail.ru>
2026-08-18 12:06:02 +0300

    changed homework

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,82 +1,86 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Cars> automobile = new ArrayList<>();
         System.out.println("Здравствуйте :) Вы попали в программу \"24 часа Ле-Мана\"," +
                 "которое рассчитывает победителя гонки зная скорость." +
                 "\nРасчет происходит по 3 автомобилям.");
-        for(int i = 1; i < 4; i++) {
-            Scanner scanner = new Scanner(System.in);
-            String name;
-            Cars car;
-            while(true) {
-                System.out.println("Введите название " + i + "-го автомобиля:");
-                name = scanner.nextLine();
-                if (!name.isEmpty()) {
-                    car = new Cars(name);
-                    break;
-                } else {
-                    System.out.print("Вы ничего не ввели, попробуйте снова.\n");
-                }
+        Track arrival = new Track();
+        Cars firstCar = new Cars(inputName(1), inputSpeed(1));
+        arrival.setWiner(firstCar);
+        Cars secondCar = new Cars(inputName(2), inputSpeed(2));
+        arrival.setWiner(secondCar);
+        Cars thirdCar = new Cars(inputName(3), inputSpeed(3));
+        arrival.setWiner(thirdCar);
+
+        System.out.println("Победитель: " + arrival.getWiner());
+    }
+
+    static String inputName(int number){
+        Scanner scanner = new Scanner(System.in);
+        String name = "";
+        while(name.isEmpty()) {
+            System.out.println("Введите название " + number + "-го автомобиля:");
+            name = scanner.nextLine();
+            if(name.isEmpty()){
+                System.out.print("Вы ничего не ввели, попробуйте снова.\n");
+            }else{
+                break;
             }
-            int speed;
-            while (true) {
-                System.out.println("Введите скорость " + i + "-го автомобиля (" +
-                        "скорость не должна быть ниже 1 и выше 250):");
-                //Подсмотрел обработку некорректного ввода чисел (текст) у ИИ
-                //не помню, чтобы такое объясняли в курсе, все остальное сделал сам
-                if(scanner.hasNextInt()){
-                    speed = scanner.nextInt();
-                    if (speed <= 0 || speed > 250) {
-                        System.out.println("Неверно введена скорость, попробуйте еще раз");
-                    }else {
-                        break;
-                    }
-                }else {
+        }
+        return name;
+    }
+
+    static int inputSpeed(int number){
+        Scanner scanner = new Scanner(System.in);
+        int speed = -1;
+        while (speed <= 0 || speed > 250) {
+            System.out.println("Введите скорость " + number + "-го автомобиля (" +
+                    "скорость не должна быть ниже 1 и выше 250):");
+            //Подсмотрел обработку некорректного ввода чисел (текст) у ИИ
+            //не помню, чтобы такое объясняли в курсе, все остальное сделал сам
+            if(scanner.hasNextInt()){
+                speed = scanner.nextInt();
+                if (speed <= 0 || speed > 250) {
                     System.out.println("Неверно введена скорость, попробуйте еще раз");
                 }
+            }else {
+                System.out.println("Неверно введена скорость, попробуйте еще раз");
             }
-            car.setSpeed(speed);
-            automobile.add(car);
         }
-
-        Track arrival = new Track(automobile);
-        System.out.println("Победитель: " + arrival.getWiner());
+        return speed;
     }
 
     public static class Cars{
         private final String name;
-        private int speed;
+        private final int speed;
 
-        Cars(String name){
+        Cars(String name, int speed){
             this.name = name;
-        }
-
-        void setSpeed(int speed){
             this.speed = speed;
         }
         int getSpeed(){
             return this.speed;
         }
-
-        String getName(){return this.name; }
+        String getName(){
+            return this.name;
+        }
     }
 
     public static  class Track{
-        private final String Winer;
+        private String Winer;
+        private int speed;
 
-        Track(ArrayList<Cars> cars){
-            String winer = "";
-            int speed = 0;
-            for(Cars car : cars){
-                if(car.getSpeed() >= speed){
-                    winer = car.getName();
-                }
+        Track(){
+            this.Winer = "";
+            this.speed = -1;
+        }
+
+        void setWiner(Cars car){
+            if(car.getSpeed() >= this.speed){
+                this.Winer = car.getName();
+                this.speed = car.getSpeed();
             }
-
-            this.Winer = winer;
         }
 
         String getWiner(){
